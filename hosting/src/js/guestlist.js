1a7601e62009ae8dcafdba1f30a3dc58f3ab4aaa
jonnilundy <jonathan.lundy@liferay.com>
2017-06-27 20:12:09 -0700

    initial changes for reception

--- hosting/src/js/guestlist.js
+++ hosting/src/js/guestlist.js
@@ -23,8 +23,6 @@ function signOut() {
 var rsvpList = document.querySelector('.rsvpList');
 var rsvpNotes = document.querySelector('.rsvpNotes');
 var guestListTotal = document.querySelector('#guestListTotal');
-var glutonListTotal = document.querySelector('#glutonListTotal');
-var dairyListTotal = document.querySelector('#dairyListTotal');
 
 var data = WeDeploy.data('data.portland-reception.wedeploy.io');
 
@@ -52,8 +50,6 @@ function appendRsvp(rsvp) {
 			'<td class="name">'+guest.first_name+' '+guest.last_name+'</td>' +
 			'<td class="attend">'+guest.attend+'</td>' +
 			'<td class="plus_one">'+guest.plus_one+'</td>' +
-			'<td class="gluten">'+guest.gluten+'</td>' +
-			'<td class="dairy">'+guest.dairy+'</td>' +
 			'<tr>'
 	});
 
@@ -78,22 +74,12 @@ function appendNotes(rsvp) {
 
 function countRsvp(rsvp) {
 	var guestCount = 0;
-	var glutenCount = 0;
-	var dairyCount = 0;
 
 	rsvp.forEach(function(guest) {
 		if (guest.plus_one && guest.attend === "Yes") {
 			guestCount = guestCount + parseInt(guest.plus_one, 10);
 		}
-		if (guest.gluten) {
-			glutenCount = glutenCount + parseInt(guest.gluten, 10);
-		}
-		if (guest.dairy) {
-			dairyCount = dairyCount + parseInt(guest.dairy, 10);
-		}
 	});
 
 	guestListTotal.innerHTML = guestCount;
-	glutonListTotal.innerHTML = glutenCount;
-	dairyListTotal.innerHTML = dairyCount;
 }
